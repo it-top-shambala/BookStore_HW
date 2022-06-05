5184c47cc95ae10303bc257fd970783e5d5ab1df
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-06-05 19:31:11 +0400

    Create model data

---
+++ BookStore.App/BookStore.App/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.App
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string NameBook { get; set; }
+        public string Genre { get; set; }
+        public int Pages { get; set; }
+
+        public int Year { get; set; }
+        
+        public int CostPrice { get; set; }
+        public int SellingPrice { get; set; }
+        public int Amount { get; set; }
+    }
+}
