6e3fe46249ff4802990de4acaaff9363a5f830cb
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-06-08 20:42:12 +0400

    update

---
+++ BookStore.App/BookStore.DB/CRUDs/BookCrud.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.App;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.DB.CRUDs
+{
+    public class BookCrud : DataBase
+    {
+        public BookCrud () : base() { }
+        public void ADDBook(Book book)
+        {
+           this.Books.Add(book);
+          
+            this.SaveChanges();
+        }
+
+
+    }
+}
