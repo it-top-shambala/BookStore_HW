4927f17ade0672985924f27a973ed12a5fdef783
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-06-11 11:30:48 +0400

    ADD NEW class model Stock !not included in the project

---
+++ BookStore.App/BookStore.DB/Models/Stock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BookStore.DB.Models
+{
+    [Table ("tab_stock") ]
+    class Stock
+    {
+        [Column ("id")]
+        public int Id { get; set; }
+        [Column("amount")]
+        public int Amount { get; set; }
+    }
+}
